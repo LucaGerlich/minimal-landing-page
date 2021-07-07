f6f4e378061e2434fae13aaa021cfc8bf73883eb
Luca <Luca@lucagerlich.de>
2021-07-08 00:41:17 +0200

    about.jsx....

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import NavbarInAbout from "../../components/NavbarInAbout";
-import "../../Style/about.css"
+import Footer from "../../components/Footer";
+import "../../Style/about.css";
 
 export default function abbout() {
     return (
         <div>
             <NavbarInAbout></NavbarInAbout>
-            <div>
-                <section className="about">
-                <h1>
-                Hi
-                <br />
-                i'm 
-                <span className="highlight"> Luca Gerlich</span>
-                <br />
-                A 
-                <span className="highlight"> developer</span>
-                <br />
-                based in
-                <span className="hightlight-light"> Seligenstadt, Germany</span>
-                .
+
+            <div className="about">
+                <h1 className="Text">
+                    Hi
+                    <br />
+                    i'm
+                    <span> Luca Gerlich.</span>
+                    <br />A<span> developer</span>
+                    <br />
+                    based in
+                    <span> Seligenstadt, Germany.</span>
                 </h1>
-            </section>
             </div>
+            <div class="container">
+                <div class="center">
+                    <p>I am vertically and horizontally centered.</p>
+                </div>
+            </div>
+            <Footer></Footer>
         </div>
     );
 }
