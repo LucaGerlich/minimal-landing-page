c0a835767f671d8cb629069b26faea1a013b2de9
Luca <Luca@lucagerlich.de>
2021-08-08 16:56:41 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: "https://www.yourdomain.tld",
-        title: "MinimalLandingPage",
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "MinimalLandingPage",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        title: `Luca Gerlich`,
+        name: `Luca Gerlich`,
+        short_name: `Luca Gerlich`,
+        start_url: `/`,
+        display: `standalone`,
+        icon: `src/image/icon.png`,
+      },
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                title: `Luca Gerlich`,
-                name: `Luca Gerlich`,
-                short_name: `Luca Gerlich`,
-                start_url: `/`,
-                display: `standalone`,
-                icon: `src/image/icon.png`,
-            },
-        },
-    ],
+  ],
 };
